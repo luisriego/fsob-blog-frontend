10682bf07e6b0011995aa7ef5f78466bab8f78bf
luisriego <luisriego@hotmail.com>
2021-04-07 10:22:36 -0300

    Login working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +15,44 @@ const App = () => {
     )  
   }, [])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    loginService.login({
+      username,
+      password
+    })
+      .then(user => {
+        setUser(user)
+        setUsername('')
+        setPassword('')
+      })
+      .catch(e => console.log(e.message))
+  }
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            placeholder='Username'
+            onChange={({target}) => setUsername(target.value)} 
+          />
+        </div>
+        <div>
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            placeholder='Password'
+            onChange={({target}) => setPassword(target.value)} 
+          />
+        </div>
+        <button>Login</button>
+      </form>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
